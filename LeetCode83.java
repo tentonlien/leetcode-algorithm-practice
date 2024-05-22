ddaf8a2fe6671a792a3a565278b758d31d7eddce
Tenton Lien <tentonlien@users.noreply.github.com>
2024-05-22 22:25:26 +0800

    Update LeetCode83.java

--- LeetCode83.java
+++ LeetCode83.java
@@ -1,34 +1,9 @@
+import common.ListNode;
+
 /**
  * @author Tenton Lien
  */
 public class LeetCode83 {
-    static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            ListNode cur = this;
-            while (cur != null) {
-                sb.append(cur.val + " ");
-                cur = cur.next;
-            }
-            return sb.toString();
-        }
-    }
-
     public ListNode deleteDuplicates(ListNode head) {
         ListNode node = head;
         if (node == null) {
